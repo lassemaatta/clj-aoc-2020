52964d26a51574f1deda569fdad5da4afa8fb359
Lasse Maatta <lasse.olavi.maatta@gmail.com>
2020-12-05 11:53:41 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject aoc-2020 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns aoc-2020.core})
